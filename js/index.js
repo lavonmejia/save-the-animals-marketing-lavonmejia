ae879557d120eb911a4d4daa213c5732ebf75bd3
LaVon Mejia <lavonmejia@gmail.com>
2019-06-28 12:57:41 -0400

    fully mobile responsive

--- js/index.js
+++ js/index.js
@@ -158,3 +158,15 @@ function openCity(cityName) {
 
 
 
+  function openSuccess(cityName) {
+    var i;
+    var x = document.getElementsByClassName("success");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";  
+    }
+    document.getElementById(cityName).style.display = "block";  
+  }
+
+
+
+
